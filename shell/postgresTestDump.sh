c3a48b1e9a7cae6f0923ab2766c63f829c64d7f6
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-12-04 10:02:00 +0500

    Bug fixed in scripts for backup database.

--- shell/postgresTestDump.sh
+++ shell/postgresTestDump.sh
@@ -2,4 +2,4 @@
 #!/usr/bin/env bash
 which sh
 cd ..
-docker exec $(docker ps -q -f name=$DOCKER_POSTGRES_DB_TEST_CONTAINER_NAME) pg_dump -U $POSTGRES_USER $POSTGRES_DB_TEST_NAME > "$POSTGRES_DB_TEST_DATA_DIR/backups/$POSTGRES_DB_TEST_NAME/dump_$(date +"%F %T").sql"
+docker exec $(docker ps -q -f name=$DOCKER_POSTGRES_DB_TEST_CONTAINER_NAME) pg_dump -U $POSTGRES_USER $POSTGRES_DB_TEST_NAME > $POSTGRES_DB_TEST_DATA_DIR/backups/$POSTGRES_DB_TEST_NAME/"dump_$(date +"%F %T").sql"
