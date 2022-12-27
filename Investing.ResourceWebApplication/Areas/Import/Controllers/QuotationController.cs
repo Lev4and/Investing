3fadf95c24693854ca8a5994b4fb0f33166e5ecb
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-12-27 23:08:50 +0500

    Implemented import of quotations of product to database.

---
+++ Investing.ResourceWebApplication/Areas/Import/Controllers/QuotationController.cs
@@ -0,0 +1,30 @@
+﻿using Investing.Infrastructure.Commands;
+using Investing.ResourceWebApplication.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Investing.ResourceWebApplication.Areas.Import.Controllers
+{
+    [ApiController]
+    [Area("Import")]
+    [Route("api/import/quotation")]
+    [EnableCors(CorsExtensions.CorsPolicyName)]
+    public class QuotationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public QuotationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImportQuotation([FromBody] ImportBcsQuotation command)
+        {
+            if (command == null) return BadRequest($"The {nameof(command)} should be not null.");
+
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
